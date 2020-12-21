dcd08b1e054939d8d203394b5b507fdab0a25181
Alika <2380919258@qq.com>
2020-12-21 21:43:33 +0800

    初体验

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,25 @@
-// pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    name:'wangcai',
+    basketball:[
+      {name:'kebo',age:18,},
+      {name:'james',age:20,},
+      {name:'taidm',age:22,},
+      {name:'alika',age:17,},
+      {name:'wangs',age:25,},
+    ],
+    number:0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  addition(){
+    // this.data.number++
+    // console.log(this.data.number)  这种写法可以改变数字的大小，但是由于不是响应式的，所以无法显示出来。
+    this.setData({
+       number:this.data.number+1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  subtraction(){
+    this.setData({
+       number:this.data.number-1
+    })
   }
 })
